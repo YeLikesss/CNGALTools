6e300dc4d264c0741695997f9ac1ea35995e9e56
YeLike <93629620+YeLikesss@users.noreply.github.com>
2025-01-14 17:33:41 +0800

    [Orange - Lover] 添加封包选择框

--- 014.OrangeStudio/Lover/ConsoleExecute/Program.cs
+++ 014.OrangeStudio/Lover/ConsoleExecute/Program.cs
@@ -1,35 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
 
 namespace ConsoleExecute
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             //情人节:不见不散
 
-            //图像解码
+            using OpenFileDialog ofd = new()
             {
-                IEnumerable<string> imgPaths = args.Where(path => Path.GetExtension(path) == ".dat");
-                foreach(string imgPath in imgPaths)
+                AddExtension = true,
+                AutoUpgradeEnabled = true,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = ".dat",
+                Filter = "dat封包(*.dat)|*.dat|所有文件(*.*)|*.*",
+                Multiselect = true,
+                RestoreDirectory = true,
+                ShowHelp = false,
+                Title = "情人节:不见不散 - 选择封包",
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string imgPath in ofd.FileNames)
                 {
-                    Bitmap? bitmap = ImageDecoder.Load(imgPath);
-                    if (bitmap != null)
+                    using Bitmap? bitmap = ImageDecoder.Load(imgPath);
+                    if (bitmap is not null)
                     {
                         bitmap.Save(Path.ChangeExtension(imgPath, ".png"), ImageFormat.Png);
-                        bitmap.Dispose();
                         Console.WriteLine("图像解码成功:{0}", Path.GetFileName(imgPath));
                     }
                 }
+                Console.WriteLine("===== 情人节:不见不散 - 图像解码成功 =====");
+                Console.Read();
             }
-
-            Console.WriteLine("=======================按任意键退出======================");
-            Console.Read();
         }
     }
 }
