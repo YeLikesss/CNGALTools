f01a923e861b0e8e85754d38fc3fce7ad315eba4
YeLike <93629620+YeLikesss@users.noreply.github.com>
2025-01-27 19:15:19 +0800

    [MeetInParis] 添加文件选择对话框

--- 004.Fontainebleau/MeetInParisDumper/Rename/Program.cs
+++ 004.Fontainebleau/MeetInParisDumper/Rename/Program.cs
@@ -1,27 +1,40 @@
 ﻿using System;
 using System.IO;
+using System.Windows.Forms;
 
 namespace Rename
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-
-            DirectoryInfo ResDir = new("E:\\花都之恋\\Resources\\Extract\\PNG");
-
-            FileInfo[] ResFiles = ResDir.GetFiles();
-
-            foreach(FileInfo resFile in ResFiles)
+            using OpenFileDialog ofd = new()
             {
-                string fileNameNoExtension = Path.GetFileNameWithoutExtension(resFile.FullName);
-                if (Path.GetExtension(fileNameNoExtension) == ".pvr")
+                AddExtension = true,
+                AutoUpgradeEnabled = true,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Filter = "所有文件(*.*)|*.*",
+                Multiselect = true,
+                RestoreDirectory = true,
+                ShowHelp = false,
+                Title = "花都之恋 - pvr资源重命名",
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string resFile in ofd.FileNames)
                 {
-                    string filename = resFile.FullName.Replace(".pvr.png", ".png", StringComparison.OrdinalIgnoreCase);
-                    resFile.MoveTo(filename);
+                    string fileNameNoExtension = Path.GetFileNameWithoutExtension(resFile);
+                    if (Path.GetExtension(fileNameNoExtension) == ".pvr")
+                    {
+                        string filename = resFile.Replace(".pvr.png", ".png", StringComparison.OrdinalIgnoreCase);
+                        File.Move(resFile, filename, true);
+                    }
                 }
+                Console.WriteLine("===== 花都之恋 - 重命名成功 =====");
+                Console.Read();
             }
-
         }
     }
 }
